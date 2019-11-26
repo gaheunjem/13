c8caa43a2a85aa567c5e37d5f5397a2051e73d7b
gaheunjem <gaheunjem@sookmyung.ac.kr>
2019-11-26 15:33:34 +0900

    실습1

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -15,8 +16,13 @@ struct student{
 int main(void) {
 	
 	struct student s1={24,"gaheun",4.3};
+	s1.ID=123456;
+	strcpy(s1.name,"gaheunjem");
+	s1.grade=3.95;
 	
-	printf("%s",student);
+	printf("ID : %d\n",s1.ID);
+	printf("name : %s\n",s1.name);
+	printf("grade : %f\n",s1.grade);
 	
 	return 0;
 }
